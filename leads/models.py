7a8eb54215a129d1a3ae69dd7272d4b4a2a94da9
Chandr <cksahuu@gmail.com>
2021-11-20 23:17:15 +0530

    removes some unwanted imports

--- leads/models.py
+++ leads/models.py
@@ -1,7 +1,6 @@
-from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.deletion import CASCADE
+
 
 class User(AbstractUser):
     pass
