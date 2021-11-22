902264b4ffdcd62456c8db8f9b33a6a2dc4f366d
Chandr <cksahuu@gmail.com>
2021-11-22 17:22:58 +0530

    added views and templates

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Leads, Agent, User
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Agent)
+admin.site.register(Leads)
+
--- leads/models.py
+++ leads/models.py
@@ -11,5 +11,13 @@ class Leads(models.Model):
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.email}"
+    
